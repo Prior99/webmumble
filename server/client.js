5bbcb710921d82339b41cf9fdcc68fe7d52f06fe
Soana <andra@ruebsteck.de>
2015-08-01 19:39:24 +0200

    Replaced not working channel joining method

--- server/client.js
+++ server/client.js
@@ -16,6 +16,7 @@ Client.prototype = {
 				connection.on("voice", this.onVoice.bind(this));
 				try{
 					connection.authenticate(args.username, "");
+					this.server = args;
 				}
 				catch(e){
 					console.error(e);
@@ -38,7 +39,41 @@ Client.prototype = {
 
 	joinChannel: function(channel){
 		path = channel.split("/");
-		this.mumble.connection.joinPath(path);
+		while(path[0] === ""){
+			path.shift();
+		}
+		if(path[0] !== this.mumble.rootChannel.name){
+			this.socket.emit("mumble-error", {
+				reason: "Channel does not exist",
+				responseTo: "joinChannel",
+				channel: channel
+			});
+			return;
+		}
+		var currentChannel = this.mumble.rootChannel;
+		for(var i = 1; i < path.length; i++){
+			if(path[i] === ""){
+				continue;
+			}
+			var found = false;
+			for(var j = 0; j < currentChannel.children.length; j++){
+				if(currentChannel.children[j].name === path[i]){
+					currentChannel = currentChannel.children[j];
+					found = true;
+					break;
+				}
+			}
+			if(! found){
+				this.socket.emit("mumble-error", {
+					reason: "ChannelNotExist",
+					responseTo: "joinChannel",
+					channel: channel
+				});
+				return;
+			}
+		}
+		currentChannel.join();
+		console.log("joining channel " + channel + " as " + this.server.username);
 		this.socket.emit("channelJoined", channel);
 	},
 	
@@ -53,7 +88,9 @@ Client.prototype = {
 
 			var users = []
 			for(var i = 0; i < channel.users.length; i++){
-				users.push(channel.users[i].name);
+				users.push({
+					name: channel.users[i].name
+				});
 			}
 			channeldata = {
 				name: channel.name,
