649830db728f1aec0390fee7c57c892b79f0991a
Frederick Gnodtke <frederick@gnodtke.net>
2015-10-07 01:46:46 +0200

    Experiment now working bidirectional.

--- test.js
+++ test.js
@@ -1,44 +1,90 @@
-var Speaker = require("speaker");
-var io = require("socket.io")(http);
-var WS = require('websocket-stream');
-var http = require('http');
-var FS = require('fs');
+/*
+ * Includes
+ */
+var WS = require('ws');
 var Mumble = require('mumble');
 var Sox = require('sox-audio');
-
-Mumble.connect("92k.de", function(error, connection) {
-	connection.authenticate('webmumbletest');
-	connection.on('initialized', function() { mumbleUp(connection); });
+var PassThroughStream = require('stream').PassThrough;
+var FS = require('fs');
+/*
+ * Constants
+ */
+var PORT = 8081;
+var IP = "0.0.0.0";
+var MUMBLE_URL = "mymun.net"
+var MUMBLE_PASSWORD = "atomrofl";
+var MUMBLE_USER = "Webmumble_Test";
+var MUMBLE_CHANNEL = "The Moon";
+/*
+ * Code
+ */
+Mumble.connect(MUMBLE_URL, function(error, connection) {
+	connection.authenticate(MUMBLE_USER, MUMBLE_PASSWORD);
+	connection.on('ready', function() {
+		mumbleUp(connection);
+	});
 });
 
 function mumbleUp(connection) {
+	connection.channelByName(MUMBLE_CHANNEL).join();
 
-	var httpserv = http.createServer();
-	var server = WS.createServer({server: httpserv}, handle);
-	httpserv.listen(8081, "127.0.0.1");
+	var wss = new WS.Server({
+		port : PORT
+	});
+	wss.on('connection', handle)
+	console.log("Server running at http://" + IP + ":" + PORT + "/");
 
-	console.log('Server running at http://127.0.0.1:8081/');
+	var mumbleInputStream = connection.inputStream();
+	var websocketStreamInput = new PassThroughStream();
+	var websocketStreamOutput = new PassThroughStream();
+	var mumbleOutputStream = new PassThroughStream();
+	connection.on("voice", function(data) {
+		mumbleOutputStream.write(data);
+	});
 
-	var is = connection.inputStream();
-	//var is = FS.createWriteStream("out_48k.pcm");
-	function handle(stream) {
-		var sox = new Sox(stream)
+	function setupBrowserToMumble(ws) {
+		var sox = new Sox(websocketStreamInput)
 			.inputSampleRate('44.1k')
 			.inputBits(32)
 			.inputChannels(1)
 			.inputFileType('raw')
 			.inputEncoding('floating-point')
-		var output = sox.output(is)
+		var output = sox.output(mumbleInputStream)
 			.outputSampleRate('48k')
 			.outputEncoding('signed')
 			.outputBits(16)
 			.outputChannels(1)
 			.outputFileType('raw');
 		sox.run();
-		console.log("Stream accepted");
-		/*stream.on('data', function(chunk) {
-			var resampledData = Samplerate.resample(chunk, 44100, 48000, 1);
-			is.write(resampledData);
-		});*/
+		ws.on('message', function(data) {
+			websocketStreamInput.write(data);
+		});
+	}
+
+	function setupMumbleToBrowser(ws) {
+		var sox = new Sox(mumbleOutputStream)
+			.inputSampleRate('48k')
+			.inputBits(16)
+			.inputChannels(1)
+			.inputFileType('raw')
+			.inputEncoding('signed')
+		var output = sox.output(websocketStreamOutput)
+			.outputSampleRate('44.1k')
+			.outputBits(32)
+			.outputEncoding('floating-point')
+			.outputChannels(1)
+			.outputFileType('raw');
+		sox.run();
+		websocketStreamOutput.on('data', function(data) {
+			ws.send(data, { binary: true, mask: true });
+		});
+	}
+
+	function handle(ws) {
+		//ws.on('open', function() {
+			setupBrowserToMumble(ws);
+			setupMumbleToBrowser(ws);
+			console.log("Stream accepted");
+		//});
 	}
 }
--- testclient.js
+++ testclient.js
@@ -1,43 +1,75 @@
-var WS = require('websocket-stream');
+/*
+ * Includes
+ */
 
-var ws = WS('ws://localhost:8081')
+/*
+ * Polyfills
+ */
 
-ws.on('data', function(chunk) {
-	console.log(chunk);
-});
-//ws.write(new ArrayBuffer(1024));
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-	getUserMedia: function(c) {
-		return new Promise(function(y, n) {
-			(navigator.mozGetUserMedia || navigator.webkitGetUserMedia).call(navigator, c, y, n);
-		});
-	}
-} : null);
+navigator.getUserMedia = (navigator.getUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.webkitGetUserMedia);
+/*
+ * Code
+ */
 
-var elem = $("#test");
-var p = navigator.mediaDevices.getUserMedia({
-	audio: true
-});
-p.then(function(stream){
-	console.log("test");
-	//var audio = new AudioContext();
-	//audio.input = this.audio.context.createMediaStreamSource(stream);
-	//elem[0].src = window.URL.createObjectURL(stream);
-	//console.log("toast");
-	console.log(stream);
-	console.log(stream.getAudioTracks());
-	//stream.pipe(ws);
+//Connect to server
+var ws = new WebSocket('ws://localhost:8081');
+ws.binaryType = "arraybuffer";
+ws.onopen = function() {
+	//Get Usermedia
+	navigator.getUserMedia({ audio: true }, mediaAcquired, mediaError);
+};
+
+
+
+function mediaAcquired(stream){
+	/*
+	 * Microphone -> WebsocketSinkNode -> Server
+	 */
 	var context = new AudioContext();
 	var input = context.createMediaStreamSource(stream);
-	var node = (context.createScriptProcessor || context.createJavaScriptNode).call(context, 512, 1, 1);
-	node.onaudioprocess = function(e) {
-		console.log(e.inputBuffer);
-		ws.write(e.inputBuffer.getChannelData(0));
+	window.input = input; //Make sure garbage collector doesn't kill us.
+	var webSocketSinkNode = context.createScriptProcessor(0, 1, 1);
+	webSocketSinkNode.onaudioprocess = function(e) {
+		ws.send(e.inputBuffer.getChannelData(0));
 	};
-	input.connect(node);
-	console.log("test2");
-}.bind(this));
-p.catch(function(e) {
-	throw e;
-});
+	input.connect(webSocketSinkNode);
+	/*
+	 * Server -> WebsocketSourceNode -> Speaker
+	 */
+	var incoming = [];
+	var bufferSize = 8192;
+	var webSocketSourceNode = context.createScriptProcessor(bufferSize, 1, 1);
+	var voiceWasEnded = true;
+	ws.onmessage = function(message) {
+		var data = new Float32Array(message.data);
+		if(data.length != bufferSize) {
+			for(var i = 0; i < data.length; i += bufferSize) {
+				incoming.push(data.slice(i, i + bufferSize));
+			}
+		}
+		else {
+			incoming.push(data);
+		}
+		if(voiceWasEnded && incoming.length > 3) {
+			voiceWasEnded = false;
+		}
+		console.log(data.length / bufferSize, "buffers written. Modulo", data.length % bufferSize, "Now ", incoming.length, "in queue");
+	};
+	webSocketSourceNode.onaudioprocess = function(e) {
+		if(incoming.length && !voiceWasEnded) {
+			var data = incoming.shift();
+			e.outputBuffer.getChannelData(0).set(data);
+		}
+		else {
+			voiceWasEnded = true;
+		}
+		console.log("Took elem from queue. Now" , incoming.length, "elements in queue");
+	};
+	webSocketSourceNode.connect(context.destination);
+
+}
+
+function mediaError(err) {
+	console.error(err);
+}
