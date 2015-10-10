c17fa98ba025836b1e8704d2355278a041339af4
Frederick Gnodtke <frederick@gnodtke.net>
2015-10-10 19:30:29 +0200

    Added doc to oggchecksum.

--- workers/oggchecksum.js
+++ workers/oggchecksum.js
@@ -1,5 +1,29 @@
+/*
+ *  This file is part of Bumble, a browser based client for mumble.
+ *
+ *  Bumble is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bumble is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Bumble. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Creates checksums as specified in the OGG standard here: http://www.xiph.org/ogg/doc/framing.html
+ */
 var OggChecksum = {
 	checksumTable : [],
+	/**
+	 * Calculates the checksum for a single buffer.
+	 * @param {ArrayBuffer} data - Calculate the checksum for this buffer.
+	 */
 	getChecksum : function(data) {
 		var checksum = 0;
 		for(var i = 0; i < data.length; i++) {
